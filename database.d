359e42a5bc58103969bb158f582e526d1fc76f86
BLM <blm@blm-laptop-linux.(none)>
2012-10-19 11:09:36 -0700

    Modified query parameter binding interface

--- database.d
+++ database.d
@@ -1,14 +1,12 @@
 module adbi.database;
 
+import std.stdio;
+
 public import adbi.table;
 
 abstract class Database {
-	TableSet tables;
-	
-	/++
-	Creates a new Query object to execute a given statement
-	+/
 	Query query(const(char)[] statement);
+	TableSet tables;
 }
 
 class Column {
@@ -22,10 +20,18 @@ enum QueryStatus {
 interface Query {
 	QueryStatus advance();
 	@property size_t numCols();
+	
+	void bind(size_t index, int value);
+	void bind(size_t index, double value);
+	void bind(size_t index, const(char)[] text);
+	void bind(size_t index, const(void)[] blob);
+	
 	long getInt(size_t index);
 	double getFloat(size_t index);
 	const(char)[] getText(size_t index);
 	const(void)[] getBlob(size_t index);
+	
+	const(char)[] getColumnName(size_t index);
 }
 
 interface TableSet {
--- sqlite3/database.d
+++ sqlite3/database.d
@@ -3,7 +3,9 @@ module adbi.sqlite3.database;
 import etc.c.sqlite3;
 import std.array;
 import std.conv;
+import std.variant;
 import std.string;
+import std.traits;
 
 public import adbi.database;
 
@@ -52,8 +54,33 @@ class Sqlite3Database: Database {
 			return cast(size_t)sqlite3_column_count(statement);
 		}
 		
+		void bind(size_t index, int value) {
+			int status = sqlite3_bind_int(statement, index, value);
+			if(status)
+				{throw new Sqlite3BindError(status, value);}
+		}
+		
+		void bind(size_t index, double value) {
+			int status = sqlite3_bind_double(statement, index, value);
+			if(status)
+				{throw new Sqlite3BindError(status, value);}
+		}
+		
+		void bind(size_t index, const(char)[] text) {
+			//To do: optimize.
+			int status = sqlite3_bind_text(statement, index, text.ptr, cast(int)text.length, SQLITE_TRANSIENT);
+			if(status)
+				{throw new Sqlite3BindError(status, text);}
+		}
+		
+		void bind(size_t index, const(void)[] blob) {
+			int status = sqlite3_bind_blob(statement, index, blob.ptr, cast(int)blob.length, SQLITE_TRANSIENT);
+			if(status)
+				{throw new Sqlite3BindError(status, blob);}
+		}
+		
 		long getInt(size_t index) {
-			return cast(long)sqlite3_column_int(statement, index);
+			return cast(long)sqlite3_column_int64(statement, index);
 		}
 		
 		double getFloat(size_t index) {
@@ -66,6 +93,10 @@ class Sqlite3Database: Database {
 		
 		const(void)[] getBlob(size_t index) {
 			return sqlite3_column_blob(statement, index)[0 .. sqlite3_column_bytes(statement, index)];
+		}
+		
+		const(char)[] getColumnName(size_t index) {
+			return sqlite3_column_name(statement, index).to!string;
 		}
 	
 		~this() {
@@ -92,4 +123,10 @@ class Sqlite3Database: Database {
 
 class Sqlite3Error: Error {
 	this(int code, string msg) {super("Sqlite error " ~ to!string(code) ~ ": " ~ msg);}
+}
+
+class Sqlite3BindError: Sqlite3Error {
+	this(T)(int code, T value) {
+		super(code, `Unable to bind value "` ~ value.to!string);
+	}
 }
